9636ab41c48ec0088ef4d0b1aa10b6b64eceaa0b
healergc <iflowerpie123@gmail.com>
2026-02-21 14:47:48 -0700

    Triggers and first level
    Added the reset triggers and evferything good!

--- Assets/Scripts/New Level Trigger.cs
+++ Assets/Scripts/New Level Trigger.cs
@@ -3,13 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class NewLevelTrigger : MonoBehaviour
 {
+    public string LevelToLoad = "NewScene";
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("I Have Found A Player");
-            SceneManager.LoadScene("SecondLevel");
+            SceneManager.LoadScene(LevelToLoad);
         }
     }
 }
